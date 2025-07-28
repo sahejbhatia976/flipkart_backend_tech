a09a8d75310489f8cf9f4dd56d040ce0133f11c3
CodeSandbox <support@codesandbox.io>
2025-07-28 09:17:05 +0000

    Sandbox snapshot commit

---
+++ auth.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const bcrptjs = require("bcryptjs");
+const router  = express.router();
+
+const User = mongoose.model('User',new mongoose.Schema({email:String,password:String}));
+
+//signup router
+router.post("/auth/signup",async(req,res)=>{
+    const {email,password} = req.body;
+    const existingUser = await User.findOne({email});
+    if(existingUser){
+        return res.status(400).json({error:'User already exist'});
+    }
+    const hashedPassword = await bcrypt.hash([password]);
+    const user = new User({email,password:hashedPassword});
+    await user.save();
+    const token = jwt.sign({userID:user._id},'secret',{expiresIn:'1h'});
+    res.status(200).json({token});
+});
+
+//login router
+router.post("auth/login",async(req,res)=>{
+    const {email,password} = req.body;
+    const user = await User.findOne({email});
+    if(user && await bcrypt.compare(password,user,password));{
+        const token = jwt.sign({userID:user._id},'secret',{expiresIn:'1h'});
+    } else{
+        res.status(400).json({error:"Invalid credentials"});
+    }
+});
+
+//jwt middleware
+
+function authenticateJWT(req,res,next)
--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 
+mongoose.connect(
+  "mongodb+srv://sahejbhatia748:<Saahe123>@cluster0.bafcj5b.mongodb.net/",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 app.listen(8080, () => {
   console.log("Server is running on port 8080");
 });
