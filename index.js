506070a8a88f50b7e817cc676311690032ced446
CodeSandbox <support@codesandbox.io>
2025-07-28 08:04:29 +0000

    Sandbox snapshot commit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+app.listen(8080, () => {
+  console.log("Server is running on port 8080");
+});
