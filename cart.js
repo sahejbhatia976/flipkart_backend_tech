4b649c4b75e8496385cd9ef2c67f00be6a74852b
CodeSandbox <support@codesandbox.io>
2025-07-29 09:56:42 +0000

    Sandbox snapshot commit

--- cart.js
+++ cart.js
@@ -23,5 +23,73 @@ router.post("/cart/add", async (req, res) => {
         .status(400)
         .json({ message: "Productid and user is required" });
     }
-  } catch (err) {}
+
+    let cart = await cart.findOne({userID:user,status:'active'});
+
+    if(!cart){
+        cart = new cart({userID:user,items:[],status:'active'});
+    }
+
+    const existingItemIndex = cart.items.findIndex(item=>items.productID===productID);
+
+    if(existingItemIndex>-1){
+        cart.items[existingItemIndex].quantity+=parseInt(quantity);
+    }else{
+        cart.items.push({
+            productID,
+            quantity:parseInt(quantity)
+        });
+    }
+    cart.updateAt = new Date();
+    await cart.save();
+  } catch (err) {
+    res.status(500).json({error:"Internal server error,item has not been added"})
+  }
 });
+
+router.get('/carts',async(req,res)=>{
+    try{
+        const carts = await cart.find({});
+        res.status(200).json({
+            success:true,
+            count:cart.length,
+            data:carts
+        });
+    }catch(error){
+        console.log("Error fetching cart",error);
+        res.status(500).json({
+            success:false,
+            message:"Failed to fetch data",
+            error:error.message,
+        });
+    }
+});
+
+
+//delete route assignment
+
+router.delete("/cart/:id",async(req,res)=>{
+    try{
+        const deleteditems = await items.findByAndDelete(req.params.id);
+
+        if(!deleteditems){
+            return res.status(404).json({
+                success:false,
+                message:"Item not found"
+            });
+        }
+        res.status(200).json({
+            success:true,
+            message: "Items deleted successfully",
+            data: deleteditems
+        });
+    }catch(err){
+        res.status(500).json({
+            success:false,
+            message:"Server error",
+            error: error.message
+        })
+    }
+});
+
+module.exports = router;
