f00283b16a7ed6f0b06a55b24543c720e4355ce4
CodeSandbox <support@codesandbox.io>
2025-07-29 10:27:13 +0000

    Sandbox snapshot commit

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
+
+const { router: authRoutes, authenticateJWT } = require("./auth");
+const cartRouter = require("./cart");
+app.use(authRoutes);
+app.use(cartRouter);
 
 mongoose.connect(
   "mongodb+srv://sahejbhatia748:<Saahe123>@cluster0.bafcj5b.mongodb.net/",
@@ -12,6 +19,25 @@ mongoose.connect(
     useUnifiedTopology: true,
   }
 );
+
+app.get("/products", async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ error: "There is inernal server error" });
+  }
+});
+
+app.get('/product/:id',async(req,res)=>{
+  try{
+    const product = await Product.findById(req.params.id);
+    if(!product){
+      return res.status
+    }
+  }
+})
+
 app.listen(8080, () => {
   console.log("Server is running on port 8080");
 });
